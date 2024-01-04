8d8294c2a123a0aefff0288656471422b8f68cca
Volodymyr Mishyn <pti.mishin@gmail.com>
2024-01-04 13:21:50 +0200

    logs for production

--- src/express/routes/data.routes.ts
+++ src/express/routes/data.routes.ts
@@ -1,14 +1,17 @@
 import { Router, Request, Response } from 'express';
 import { DatabaseAccessor } from '../../_models/storage/database-accessor';
 import { DataController } from '../controllers/data.controller';
+import { Logger } from '../../_helpers/logger';
 
 export function createDataRoutes(databaseAccessor: DatabaseAccessor) {
   const dataController = new DataController(databaseAccessor);
   const router = Router();
   router.get(`/mod`, (request: Request, response: Response) => {
+    Logger.log(`GET /mod`);
     return dataController.getMoDData(request, response);
   });
   router.get(`/oryx`, (request: Request, response: Response) => {
+    Logger.log(`GET /oryx`);
     return dataController.getOryxCountryData(request, response);
   });
   return router;
